95b6ea0b51c62051009a4cd0e9e407280fc66bae
Aleksei Maslakov <lesha.maslakov@gmail.com>
2020-03-25 23:25:20 +0300

    Create links with -f option

--- install.zsh
+++ install.zsh
@@ -3,8 +3,8 @@
 DOTFILES=$(dirname ${0:a})
 
 # git
-ln -s ${DOTFILES}/.gitignore_global ~/.gitignore_global
-ln -s ${DOTFILES}/.gitconfig ~/.gitconfig
+ln -sf ${DOTFILES}/.gitignore_global ~/.gitignore_global
+ln -sf ${DOTFILES}/.gitconfig ~/.gitconfig
 
 # git-prompt
 GIT_PROMPT_URL="https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh"
@@ -17,12 +17,12 @@ source ${DOTFILES}/vscode/install.zsh
 mkdir -p ~/.vim/colors
 VIM_EIGHTIES_URL="https://raw.githubusercontent.com/lithammer/vim-eighties/master/colors/eighties.vim"
 curl ${VIM_EIGHTIES_URL} > ~/.vim/colors/eighties.vim
-ln -s ${DOTFILES}/.vimrc ~/.vimrc
+ln -sf ${DOTFILES}/.vimrc ~/.vimrc
 
 # zsh
-ln -s ${DOTFILES}/.aliases ~/.aliases
-ln -s ${DOTFILES}/.zshenv ~/.zshenv
-ln -sn ${DOTFILES}/.zsh ~/.zsh
-ln -s ${DOTFILES}/.zshrc ~/.zshrc
+ln -sf ${DOTFILES}/.aliases ~/.aliases
+ln -sf ${DOTFILES}/.zshenv ~/.zshenv
+ln -snf ${DOTFILES}/.zsh ~/.zsh
+ln -sf ${DOTFILES}/.zshrc ~/.zshrc
 
 source ${DOTFILES}/.zshrc
